bf1615a77452b4ea7b38b0b9e9d48e5f122e004f
D9nni <78950235+D9nni@users.noreply.github.com>
2023-11-05 21:02:55 +0200

    homework 600p + README.md

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::os::unix::prelude::PermissionsExt;
+use std::os::unix::prelude::{FileExt, PermissionsExt};
 /* Suggestions: 1. Write a function for every command
                 2. Start with the pwd command
                 3. Continue with the other commands that do not have parameters
 */
 //fs::File, io
-use std::{env, fs, path::Path, process,fs::Permissions};
-
+use std::{env, fs, fs::Permissions, io::Read, path::Path, process::exit};
 
 fn pwd() {
     // TODO 3: Implement the logic for pwd
@@ -22,6 +21,7 @@ fn echo(args: Vec<String>) {
         newline = 3;
     }
 
+
     for i in newline..args.len() - 1 {
         print!("{} ", args[i]);
     }
@@ -32,16 +32,21 @@ fn echo(args: Vec<String>) {
     }
 }
 fn cat(args: Vec<String>) {
+    if args.len() == 2 {
+        invalid();
+    }
     for i in 2..args.len() {
         match fs::read_to_string(args[i].clone()) {
             Ok(s) => print!("{}", s),
-            Err(_err) => process::exit(-20),
+            Err(_err) => exit(-20),
         }
     }
 }
 
 fn mkdir(args: &[String]) -> i32 {
-    //println!("mkdir name {}", args[0]);
+    if args.len() == 0 {
+        invalid();
+    }
     for i in 0..args.len() {
         match fs::create_dir(args[i].clone()) {
             Ok(_s) => (),
@@ -53,7 +58,7 @@ fn mkdir(args: &[String]) -> i32 {
 
 fn mv(args: Vec<String>) {
     if args.len() != 4 {
-        panic!("INSUFICIENTE ARGUMENTE");
+        invalid();
     }
     let source = args[2].clone();
     let dest = args[3].clone();
@@ -62,37 +67,42 @@ fn mv(args: Vec<String>) {
     }
     match fs::rename(source, dest) {
         Ok(_) => (),
-        Err(_e) => process::exit(-40),
+        Err(_e) => exit(-40),
     }
 }
 fn ln(args: Vec<String>) {
     if args.len() < 4 {
-        panic!("INSUFICIENTE ARGUmente");
+        invalid();
     }
     let mut idx = 2;
     if args[2] == "-s" || args[2] == "--symbolic" {
         idx = 3;
+    } else if args[2].starts_with("-") || args.len() == idx{
+        invalid();
     }
+
     let oldpath = args[idx].clone();
     let newpath = args[idx + 1].clone();
-
     if idx == 2 {
         match fs::hard_link(oldpath, newpath) {
             Ok(_) => (),
-            Err(_e) => process::exit(-50),
+            Err(_e) => exit(-50),
         }
     } else if idx == 3 {
         match std::os::unix::fs::symlink(oldpath, newpath) {
             Ok(_) => (),
-            Err(_e) => process::exit(-50),
+            Err(_e) => exit(-50),
         }
     }
 }
 fn rmdir(args: Vec<String>) {
+    if args.len() == 2 {
+        invalid();
+    }
     for i in 2..args.len() {
         match fs::remove_dir(args[i].clone()) {
             Ok(_) => (),
-            Err(_e) => process::exit(-60),
+            Err(_e) => exit(-60),
         }
     }
 }
@@ -100,13 +110,17 @@ fn rm(args: Vec<String>) {
     let mut rmdir_rec = false;
     let mut rmdir = false;
     let mut k = 2;
-    while args[k] == "-r" || args[k] == "-R" || args[k] == "--recursive" {
+
+    while k < args.len() && (args[k] == "-r" || args[k] == "-R" || args[k] == "--recursive") {
         rmdir_rec = true;
         k += 1;
     }
-    while args[k] == "-d" || args[k] == "--dir" {
+    while k < args.len() && (args[k] == "-d" || args[k] == "--dir") {
         rmdir = true;
         k += 1;
+    }
+    if args.len() == k {
+        invalid();
     }
     let mut exit_code = 0;
     if rmdir == false && rmdir_rec == false {
@@ -159,98 +173,87 @@ fn rm(args: Vec<String>) {
             };
         }
     }
-    process::exit(exit_code);
+    exit(exit_code);
 }
-fn ls_elementar(dir_path:&String,all:bool) ->Vec<String>{
-    
-        let p = Path::new(dir_path);
-        if p.is_dir() {
-            match p.read_dir() {
-                Ok(pa) => {
-                    let paths = pa;
-                    let mut vect: Vec<String> = Vec::new();
-                    if all {
-                        vect.push(String::from("."));
-                        vect.push(String::from(".."));
-                    }
-                    for iter in paths {
-                        if let Ok(iter) = iter {
-                            let x = iter.file_name().to_string_lossy().into_owned();
-                            if x.starts_with('.') && all==false {
-                                
-                            }
-                            else {
-                                vect.push(x);
-                            }
+fn ls_elementar(dir_path: &String, all: bool) -> Vec<String> {
+    let p = Path::new(dir_path);
+    if p.is_dir() {
+        match p.read_dir() {
+            Ok(pa) => {
+                let paths = pa;
+                let mut vect: Vec<String> = Vec::new();
+                if all {
+                    vect.push(String::from("."));
+                    vect.push(String::from(".."));
+                }
+                for iter in paths {
+                    if let Ok(iter) = iter {
+                        let x = iter.file_name().to_string_lossy().into_owned();
+                        if x.starts_with('.') && all == false {
+                        } else {
+                            vect.push(x);
                         }
                     }
-                    vect.sort();
-                    for elem in &vect {
-                        println!("{}",elem);
-                    }
-                   vect
                 }
-                Err(_) => process::exit(-80),
-    }
+                vect.sort();
+                for elem in &vect {
+                    println!("{}", elem);
+                }
+                vect
+            }
+            Err(_) => exit(-80),
+        }
     } else if p.is_file() {
-        println!("{}",dir_path);
-        process::exit(0);
+        println!("{}", dir_path);
+        exit(0);
+    } else {
+        //if file doesn't exist
+        exit(-80);
     }
-    else { //if file doesn't exist
-        process::exit(-80);
-    }
-    
 }
-fn ls_rec(dir_path:&String,all:bool) {
+fn ls_rec(dir_path: &String, all: bool) {
     let p = Path::new(dir_path);
-    if p.is_dir(){
-        println!("{}:",dir_path);
+    if p.is_dir() {
+        println!("{}:", dir_path);
     }
-    let mut vect = ls_elementar(&dir_path,all);
-   // println!();
-    for elem in &mut vect {
-        if elem!="." && elem!=".." {
+    let mut vect = ls_elementar(&dir_path, all);
 
-        
-        let p2=Path::new(&elem);
-        let p_dir=p.join(&p2);
-        let dir_p=String::from(p_dir.to_string_lossy());
-        if p_dir.is_dir() {
-            ls_rec(&dir_p, all);
+    for elem in &mut vect {
+        if elem != "." && elem != ".." {
+            let p2 = Path::new(&elem);
+            let p_dir = p.join(&p2);
+            let dir_p = String::from(p_dir.to_string_lossy());
+            if p_dir.is_dir() {
+                ls_rec(&dir_p, all);
+            }
         }
     }
-    }
-
 }
 fn ls(args: &[String]) {
     //ls primeste cel mult un director ca argument
-    let mut k=0;
-    let mut all=false;
+    let mut k = 0;
+    let mut all = false;
     let mut rec = false;
     let length = args.len();
     let current_dir;
-    let params:Vec<&str>=vec!["-a","--all","-R","--recursive"];
-    while k<args.len() && params.contains(&args[k].as_str()){
-        
+    while k < args.len() && args[k].starts_with("-") {
         match args[k].as_str() {
-            "-a" | "--all" => all=true,
-            "-R" | "--recursive" => rec=true,
-            _ => (),
+            "-a" | "--all" => all = true,
+            "-R" | "--recursive" => rec = true,
+            _ => invalid(),
         }
-        k+=1;
+        k += 1;
     }
 
-
-    if k==length {
+    if k == length {
         current_dir = String::from(".");
-    }
-    else {
+    } else {
         current_dir = args[k].clone();
     }
     if rec {
-        ls_rec(&current_dir,all);
+        ls_rec(&current_dir, all);
     } else {
-        ls_elementar(&current_dir,all);
+        ls_elementar(&current_dir, all);
     }
 }
 fn cp_helper(source: &[String], dest: String, errno: &mut i32) {
@@ -291,7 +294,7 @@ fn cp_helper(source: &[String], dest: String, errno: &mut i32) {
                     let err2 = mkdir(&vec_dest[0..]);
                     if err2 == -30 {
                         // println!("Failed mkdir!");
-                        process::exit(-90);
+                        exit(-90);
                     }
                     // println!("Se apeleaza functia recursiva...! cu dest2: {}", dest2);
 
@@ -339,38 +342,37 @@ fn cp_helper(source: &[String], dest: String, errno: &mut i32) {
 }
 fn cp(args: Vec<String>) {
     if args.len() < 4 {
-        panic!("Invalid command");
+        invalid();
     }
     let mut errno = 0;
     let len = args.len();
     let mut k = 2;
     let mut rec = false;
-    while args[k] == "-r" || args[k] == "-R" || args[k] == "--recursive" {
+    while k < args.len() && (args[k] == "-r" || args[k] == "-R" || args[k] == "--recursive") {
         rec = true;
         k += 1;
     }
+    if args.len() == k || args[k].starts_with("-") {
+        invalid();
+    }
     let source = &args[k..len - 1];
     let dest = args[len - 1].clone();
-    // for i in 0..source.len() {
-    //     println!("Sources: {} ", source[i]);
-    // }
-    // println!("Destination: {}", dest);
+
     if source.len() == 1 && source[0] == dest {
         return ();
     }
-
+    //daca sunt mai multe argumente si destinatia nu e director
     let p = Path::new(&dest);
     if source.len() > 1 {
         if !p.is_dir() {
-            //println!("File {} is not a directory!", &dest);
-            process::exit(-90);
+            exit(-90);
         }
     } else {
         let p2 = Path::new(&source[0]);
         if p2.is_dir() && p.exists() && !p.is_dir() {
-            process::exit(-90);
+            exit(-90);
         } else if p2.is_dir() && !p.exists() {
-            errno = 777; // rename directory flag
+            errno = 777; // rename and cp directory flag
         }
     }
     if rec {
@@ -409,147 +411,140 @@ fn cp(args: Vec<String>) {
         }
     }
     //println!("Final errno is: {}", errno);
-    process::exit(errno);
+    exit(errno);
 }
-fn touch(args:&[String]) {
+fn touch(args: &[String]) {
+    if args.len() == 0 {
+        invalid();
+    }
+    let mut k = 0;
+    let mut a = false;
+    let mut c = true;
+    let mut m = false;
+    while k < args.len() && args[k].starts_with("-") {
+        match args[k].as_str() {
+            "-a" => a = true,
+            "-c" | "--no-create" => c = false,
+            "-m" => m = true,
+            _ => invalid(),
+        }
+        k += 1;
+    }
+    if k == args.len() {
+        invalid();
+    }
+    let path = Path::new(&args[k]);
+    match fs::OpenOptions::new()
+        .create(c)
+        .read(true)
+        .write(true)
+        .open(path)
+    {
+        Ok(f) => {
+            let mut g = f;
 
-    // fn touch(path: &Path) -> io::Result<()> {
-        if args.len()==0 {
-            panic!("Invalid command");
-        }
-        let params:Vec<&str>=vec!["-a","-c", "--no-create","-m"];
-        let mut k=0;
-        let mut a=false;
-        let mut c = false;
-        let mut m = false;
-        while k<args.len() && params.contains(&args[k].as_str()){
-            match args[k].as_str() {
-                "-a" => a = true,
-                "-c" | "--no-create" => c = true,
-                "-m" => m = true,
-                _ => (),
+            if a && !m {
+                let mut buf: [u8; 1] = [0];
+                let _result = g.read(&mut buf);
+            } else {
+                let mut buf: [u8; 1] = [0];
+                let _result = g.read(&mut buf);
+                let _result = g.write_at(&buf, 0);
             }
-            k+=1;
         }
-        if k==args.len() {
-            panic!("Invalid command");
-        }
-        let path = Path::new(&args[k]);
-        if !path.is_file() {
-            process::exit(-100);
-        }
-        
-        
-        // let metadata = path.metadata().unwrap();
-        // // metadata.accessed().
-        // // metadata.created()
-        // // metadata.modified()
-        match fs::OpenOptions::new().create(!c).write(!m).open(path) {
-            Ok(_) => (),
-            Err(_e) => {
-                println!("Eroare!");
-                process::exit(-100);
+        Err(_e) => {
+            if c {
+                exit(-100);
             }
-        };
-    // https://doc.rust-lang.org/stable/rust-by-example/std_misc/fs.html
+        }
+    }
 }
-fn chmod(args:&[String]) {
-    //Formatul generic este: u/g/o/a +/- r/w/x. 
-    if args.len()!=2 {
-        println!("Invalid command");
-        process::exit(-1);
+fn chmod(args: &[String]) {
+    //Formatul generic este: u/g/o/a +/- r/w/x.
+    if args.len() != 2 {
+        invalid();
     }
 
-    
-    let perm_nr=args[0].parse::<u32>();
+    let perm_nr = u32::from_str_radix(&args[0].as_str(), 8);
     if let Ok(perm_nr) = perm_nr {
-        if(perm_nr>777) {
-            process::exit(-25);
-        }
-        println!("{}",perm_nr);
         let path = Path::new(&args[1]);
         let perms = fs::Permissions::from_mode(perm_nr);
         fs::set_permissions(path, perms).unwrap();
-        process::exit(0);
+        exit(0);
     }
     //pt formatul ugo+-rwx
     else {
-        let pstr:Vec<char>=args[0].chars().collect();
+        let pstr: Vec<char> = args[0].chars().collect();
         let mut k = 0;
-        let mut persons=vec![0,0,0];
+        let mut persons = vec![0, 0, 0];
         //parseaza argumentele pana la +
-        while k<pstr.len() && pstr[k]!='+' && pstr[k]!='-' {
+        while k < pstr.len() && pstr[k] != '+' && pstr[k] != '-' {
             match pstr[k] {
-                'a'=> {persons[0]=1;
-                persons[1]=1;
-                persons[2]=1;}
-                'u' => persons[0]=1,
-                'g' => persons[1]=1,
-                'o' => persons[2]=1,
-                _ =>   invalid(-25),
+                'a' => {
+                    persons[0] = 1;
+                    persons[1] = 1;
+                    persons[2] = 1;
+                }
+                'u' => persons[0] = 1,
+                'g' => persons[1] = 1,
+                'o' => persons[2] = 1,
+                _ => invalid(),
             }
-            k+=1;
+            k += 1;
         }
-        if k==pstr.len() {
-            invalid(-25);
+        if k == pstr.len() {
+            invalid();
         }
-        let mut add=false;
-        if pstr[k]=='+' {
-            add=true;
-        } else if pstr[k]!='-' {
-            invalid(-25);
+        let mut add = false;
+        if pstr[k] == '+' {
+            add = true;
+        } else if pstr[k] != '-' {
+            invalid();
         }
-        k+=1;
-        let mut mode = vec![0,0,0];
-        while k<pstr.len() {
+        k += 1;
+        let mut mode = vec![0, 0, 0];
+        while k < pstr.len() {
             match pstr[k] {
-                'r' => mode[0]=1,
-                'w' => mode[1]=1,
-                'x' => mode[2]=1,
-                _ => invalid(25),
+                'r' => mode[0] = 1,
+                'w' => mode[1] = 1,
+                'x' => mode[2] = 1,
+                _ => invalid(),
             }
-            k+=1;
-        }  
-       
-        let mode_digit=mode[0]*4+mode[1]*2+mode[2]*1;
-        let mode_nr=mode_digit*(100*persons[0]+10*persons[1]+persons[2]);
-        println!("mode_nr {}",mode_nr);
+            k += 1;
+        }
+
+        let mode_digit = mode[0] * 4 + mode[1] * 2 + mode[2] * 1;
+        let mode_nr = mode_digit * (64 * persons[0] + 8 * persons[1] + persons[2]);
         let path = Path::new(&args[1]);
-        let metadata=fs::metadata(path).unwrap();
-        let c_perms=metadata.permissions();
+        let metadata = fs::metadata(path).unwrap();
+        let c_perms = metadata.permissions();
         let c_mode = c_perms.mode();
-        println!("c_mode {}", c_mode);
         let new_mode;
         if add {
-        new_mode = c_mode | mode_nr; // +
-        }else {
-        new_mode = (!mode_nr) & c_mode; // -
-    }
-        println!("{}", new_mode);
+            new_mode = c_mode | mode_nr; // +
+        } else {
+            new_mode = (!mode_nr) & c_mode; // -
+        }
         let new_perms = Permissions::from_mode(new_mode);
-        fs::set_permissions(path, new_perms);
+        fs::set_permissions(path, new_perms).unwrap(); //unsafe
     }
-   
-
-
 }
 
-fn invalid(code:i32) {
+fn invalid() {
     println!("Invalid command");
-    process::exit(code);
+    exit(-1);
 }
 fn main() {
     // TODO 1: Read the command line arguments
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
-        println!("Please enter a command!");
-        return ();
+        invalid();
     }
     match args[1].as_str() {
         "pwd" => pwd(),
         "echo" => echo(args),
         "cat" => cat(args),
-        "mkdir" => process::exit(mkdir(&args[2..])),
+        "mkdir" => exit(mkdir(&args[2..])),
         "mv" => mv(args),
         "ln" => ln(args),
         "rmdir" => rmdir(args),
@@ -558,7 +553,6 @@ fn main() {
         "cp" => cp(args),
         "touch" => touch(&args[2..]),
         "chmod" => chmod(&args[2..]),
-        _ => {println!("Invalid command");
-        process::exit(-1);}
+        _ => invalid(),
     }
 }
